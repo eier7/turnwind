33030be9d0110385bcfa29e1eb2f2d97e6fd4452
eier7 <eivind.ervik@gmail.com>
2018-06-15 08:41:56 +0000

    initial

---
+++ turnwind.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+import RPi.GPIO as GPIO
+from time import sleep
+import serial
+import re
+import sys
+
+def checksum(sentence):
+    calc_cksum = 0
+    for s in sentence:
+        calc_cksum ^= ord(s)
+    return '*'+hex(calc_cksum)[-2:].upper()
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP) #SWITCH
+GPIO.setup(18, GPIO.OUT) #LED
+
+pushed = False
+serialinit = False
+
+ser = serial.Serial("/dev/ttyAMA0", 38400)
+
+while True:
+    if GPIO.input(4) == False and not pushed:
+        pushed = True
+    if GPIO.input(4) == True and pushed:
+        pushed = False
+    try:
+        line = ser.readline()
+        lineb = line
+        line = line.decode("ISO-8859-1")
+        if pushed:
+            ser.write(lineb)
+            print(line)
+            if re.match("^\$..MWV", line):
+                s = line.split(",")
+                heading = float(s[1])
+                heading = (heading + 180) % 360
+                s[1] = "%.2f" % heading
+                chk = checksum(','.join(s)[1:])
+                ser.write(bytes(','.join(s)+chk+"\r\n", "UTF-8"))
+            else:
+                ser.write(bytes(line, "UTF-8"))
+        else:
+            ser.write(bytes(line, "UTF-8"))
+    except:
+        pass
+        sleep(.2)
+
+    if pushed:
+        GPIO.output(18, 1)
+    else:
+        GPIO.output(18, 0)
+
